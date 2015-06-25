0b766e5479e00989f781100f3fb3e5a5288f6a6c
Wojciech Sudol <wojciech.sudol@pl.ibm.com>
2015-06-25 22:06:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# NodeHelloWorldOnGitHub
+
+[![Deploy to Bluemix](http://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/wsudol/NodeHelloOnGitHub.git)
+
---
+++ app.js
@@ -0,0 +1,9 @@
+/*global process require*/
+
+var port = (process.env.VCAP_APP_PORT || 3000);
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('NodeAppOnPrvGithub 2015-06-25-11');
+}).listen(port);
